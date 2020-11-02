c58f7b2a803803e7cdde1fcd40a67a20dc0e4a48
HWFBL <russinho00@gmail.com>
2020-11-02 18:45:51 +0100

    update gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -194,7 +194,7 @@ module.exports = {
     'gatsby-plugin-catch-links',
     'gatsby-plugin-react-helmet',
     {
-      resolve: 'gatsby-plugin-sass@next',
+      resolve: 'gatsby-plugin-sass',
       options: {
         postCssPlugins: [...postCssPlugins],
         cssLoaderOptions: {
